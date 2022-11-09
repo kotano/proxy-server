10da5b96aa3d7868b78004afc7b3333fd5552fb4
root <root@ejdaaubtix>
2022-11-09 11:46:51 +0000

    Configure simple proxy

--- index.js
+++ index.js
@@ -1,16 +1,61 @@
-require('dotenv').config()
-var http = require('http');
-var httpProxy = require('http-proxy');
-var proxy = httpProxy.createProxyServer({});
-var fs = require('node:fs');
+/* eslint-disable @typescript-eslint/no-var-requires */
+// https://stackoverflow.com/a/63602976/470749
+require('dotenv').config();
+const express = require('express');
 
-http.createServer(function (req, res) {
-  proxy.web(req, res, {
-    target: process.env.PROXY_URL,
-    secure: false,
-    ssl: {
-      key: fs.readFileSync(process.env.SSL_KEY, 'utf8'),
-      cert: fs.readFileSync(process.env.SSL_CERT, 'utf8')
+const app = express();
+const https = require('https');
+const http = require('http');
+
+const targetUrl = process.env.TARGET_URL || 'https://jsonplaceholder.typicode.com'; // Run localtunnel like `lt -s rscraper -p 8080 --print-requests`; then visit https://yourname.loca.lt/todos/1 .
+const proxyServerPort = process.env.PROXY_SERVER_PORT || 8080;
+
+// eslint-disable-next-line max-lines-per-function
+app.use('/', function (clientRequest, clientResponse) {
+  const parsedHost = targetUrl.split('/').splice(2).splice(0, 1).join('/');
+  let parsedPort;
+  let parsedSSL;
+  if (targetUrl.startsWith('https://')) {
+    parsedPort = 443;
+    parsedSSL = https;
+  } else if (targetUrl.startsWith('http://')) {
+    parsedPort = 80;
+    parsedSSL = http;
+  }
+  const options = {
+    hostname: parsedHost,
+    port: parsedPort,
+    path: clientRequest.url,
+    method: clientRequest.method,
+    headers: {
+      'User-Agent': clientRequest.headers['user-agent'],
+    },
+  };
+
+  const serverRequest = parsedSSL.request(options, function (serverResponse) {
+    let body = '';
+    if (String(serverResponse.headers['content-type']).indexOf('text/html') !== -1) {
+      serverResponse.on('data', function (chunk) {
+        body += chunk;
+      });
+
+      serverResponse.on('end', function () {
+        // Make changes to HTML files when they're done being read.
+        // body = body.replace(`example`, `Cat!`);
+
+        clientResponse.writeHead(serverResponse.statusCode, serverResponse.headers);
+        clientResponse.end(body);
+      });
+    } else {
+      serverResponse.pipe(clientResponse, {
+        end: true,
+      });
+      clientResponse.contentType(serverResponse.headers['content-type']);
     }
-  })
-}).listen(process.env.PROXY_PORT || 3008);
+  });
+
+  serverRequest.end();
+});
+
+app.listen(proxyServerPort);
+console.log(`Proxy server listening on port ${proxyServerPort}`);
